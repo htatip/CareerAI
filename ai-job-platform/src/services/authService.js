29e69b17d23933df68ba070b9996bbee79b8bcd9
Harshi <harshitsaitatiparthi@gmail.com>
2026-03-23 00:24:56 +0530

    react api callingp[ath changed

--- ai-job-platform/src/services/authService.js
+++ ai-job-platform/src/services/authService.js
@@ -1,9 +1,9 @@
 import API from "../api/axios";
 
-export const register = (data) => API.post("/auth/register", data);
+export const register = (data) => API.post("/api/auth/register", data);
 
 export const login = async ({ email, password }) => {
-  const res = await API.post("/auth/login", { email, password });
+  const res = await API.post("/api/auth/login", { email, password });
   const token = res.data.data;
   localStorage.setItem("token", token);
   return token;
